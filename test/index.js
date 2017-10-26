58c772afcb6040951919af4973636d1885c38d4c
Bryan Morris <bryan@internalfx.com>
2017-10-25 18:34:13 -0700

    tests

--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@ let path = require('path')
 let Promise = require('bluebird')
 let fs = Promise.promisifyAll(require('fs'))
 let PostGrid = require('../index')
+let ifxUtils = require('../lib/ifx-utils.js')
 
 describe('initBucket()', function () {
   it('complete without error', async function () {
@@ -23,11 +24,66 @@ describe('writeFile()', function () {
   })
 
   it('complete without error', async function () {
-    let buffer = await fs.readFileAsync(path.join(__dirname, 'files', 'shore.mp4'))
+    let buffer = await fs.readFileAsync(path.join(__dirname, 'files', 'enterprise.jpg'))
     await pGrid.writeFile({
-      filename: '/videos/shore.mp4',
-      buffer: buffer,
-      tags: ['drone', 'ocean', 'flying']
+      filename: '/pictures/enterprise.jpg',
+      buffer: buffer
     })
+  })
+})
+
+describe('createWriteStream()', function () {
+  let pGrid
+
+  before(async function () {
+    pGrid = PostGrid({db: 'postgrid', username: 'test', password: 'test'})
+    await pGrid.initBucket({dropTables: true})
+  })
+
+  it('complete without error', async function () {
+    let rStream = await fs.createReadStream(path.join(__dirname, 'files', 'enterprise.jpg'))
+    let wStream = pGrid.createWriteStream({
+      filename: '/pictures/enterprise.jpg'
+    })
+
+    rStream.pipe(wStream)
+
+    await ifxUtils.writeStreamPromise(wStream)
+  })
+})
+
+describe('getFile()', function () {
+  let pGrid
+
+  before(async function () {
+    pGrid = PostGrid({db: 'postgrid', username: 'test', password: 'test'})
+    await pGrid.initBucket({dropTables: true})
+    let buffer = await fs.readFileAsync(path.join(__dirname, 'files', 'enterprise.jpg'))
+    await pGrid.writeFile({
+      filename: '/pictures/enterprise.jpg',
+      buffer: buffer
+    })
+  })
+
+  it('complete without error', async function () {
+    await pGrid.getFile({filename: '/pictures/enterprise.jpg'})
+  })
+})
+
+describe('readFile()', function () {
+  let pGrid
+
+  before(async function () {
+    pGrid = PostGrid({db: 'postgrid', username: 'test', password: 'test'})
+    await pGrid.initBucket({dropTables: true})
+    let buffer = await fs.readFileAsync(path.join(__dirname, 'files', 'enterprise.jpg'))
+    await pGrid.writeFile({
+      filename: '/pictures/enterprise.jpg',
+      buffer: buffer
+    })
+  })
+
+  it('complete without error', async function () {
+    await pGrid.readFile({filename: '/pictures/enterprise.jpg'})
   })
 })
